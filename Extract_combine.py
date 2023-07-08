768c4b702c8ff54c8d2ddb75521450e4554fce09
Atharva Martiwar <atharvamartiwar12345@gmail.com>
2023-07-08 22:57:56 +0530

    AQI&ClimateCombine.py

--- Extract_combine.py
+++ Extract_combine.py
@@ -1,4 +1,4 @@
-from Plot_AQI import avg_data_2013,avg_data_2014,avg_data_2015,avg_data_2016
+from BangaloreAQI2013-2018 import avg_data_2013,avg_data_2014,avg_data_2015,avg_data_2016
 import requests
 import sys
 import pandas as pd
